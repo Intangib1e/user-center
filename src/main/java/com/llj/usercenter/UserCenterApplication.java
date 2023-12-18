d770c257f237a1725a6533607bf107b1ddb13260
刘露杰 <lljsln@163.com>
2023-12-18 13:20:35 +0800

    更改了一些配置项

--- src/main/java/com/llj/usercenter/UserCenterApplication.java
+++ src/main/java/com/llj/usercenter/UserCenterApplication.java
@@ -1,11 +1,13 @@
 package com.llj.usercenter;
 
+import com.feiniaojin.gracefulresponse.EnableGracefulResponse;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 @MapperScan(value = "com.llj.usercenter.mapper")
+@EnableGracefulResponse// 开启统一结果返回
 public class UserCenterApplication {
 
     public static void main(String[] args) {
