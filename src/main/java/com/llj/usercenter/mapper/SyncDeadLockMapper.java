042b7bd40789bb08621bf701aae6e9b8932f5ec6
刘露杰 <lljsln@163.com>
2024-03-05 23:22:39 +0800

    模拟锁增加mapper

---
+++ src/main/java/com/llj/usercenter/mapper/SyncDeadLockMapper.java
@@ -0,0 +1,13 @@
+package com.llj.usercenter.mapper;
+
+import com.llj.usercenter.model.domain.User;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * @Author 刘露杰
+ * @Date 2024/2/29 14:58
+ * @Description:
+ */
+@Mapper
+public interface SyncDeadLockMapper {
+}
